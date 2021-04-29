34300ea5d19ff9638e48402bb3d07655f940afd3
tgalkovskiy <80895083+GalkovskiyTimur@users.noreply.github.com>
2021-04-29 16:20:11 +0300

    add project

---
+++ Assets/Scripts/Drift_Zone.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Drift_Zone : MonoBehaviour
+{
+    public float Angle_rot;
+    public float Change_Speed =1;
+    public bool Rot_rigth;
+    public bool Rot_left;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        DOTween.To(() => other.GetComponent<MainDir>().Speed_Z, x => other.GetComponent<MainDir>().Speed_Z = x,
+            other.GetComponent<MainDir>().Speed_Z / Change_Speed, 1);
+        
+        if (Rot_rigth && !Rot_left)
+        {
+            Angle_rot *= 1;
+        }
+        if (Rot_left && !Rot_rigth)
+        {
+            Angle_rot *= -1;
+        }
+        other.transform.DOLocalRotateQuaternion(Quaternion.Euler(0,Angle_rot,0), 1f);
+        
+        //other.transform.DORotateQuaternion();
+        //other.transform.DORotate(this.transform.eulerAngles, A, 1);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        /*if (Rot_rigth && !Rot_left)
+        {
+           other.transform.Rotate(0,Angle_rot,0); 
+        }
+        if (Rot_left && !Rot_rigth)
+        {
+            other.transform.Rotate(0,-Angle_rot,0); 
+        }*/
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        DOTween.To(() => other.GetComponent<MainDir>().Speed_Z, x => other.GetComponent<MainDir>().Speed_Z = x,
+            other.GetComponent<MainDir>().Speed_Z * Change_Speed, 1);
+    }
+}
---
+++ Assets/Scripts/MainDir.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainDir : MonoBehaviour
+{
+   
+    public float Speed_X = 7;
+    public float Speed_Z = 5;
+    
+    private float x_dir;
+    protected float z_dir = 1;
+
+    private Rigidbody Player;
+    private void Awake()
+    {
+        Player = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        {
+            x_dir = 0;
+            if (Input.GetKey(KeyCode.D))
+            {
+                x_dir = 1;
+
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                x_dir = -1;
+
+            }
+            Vector3 Global_Dir = new Vector3(x_dir * Speed_X * Time.deltaTime, 0, z_dir * Speed_Z * Time.deltaTime);
+            //this.transform.localPosition += Global_Dir;
+            transform.Translate(Global_Dir, Space.Self);
+             
+            /*Vector3 GlobalDir = Vector3.forward;
+            GlobalDir += new Vector3()*/
+            
+            //Player.AddForce(Global_Dir*Speed_Z);
+            /*if (Player.velocity.magnitude < 20)
+            {
+              Player.AddRelativeForce(Global_Dir*Speed_Z);  
+            }*/
+            
+           
+            /*if (Car.transform.localPosition.z > Hint)
+            {
+                New_Road();
+            }*/
+            //Player.AddRelativeForce(Vector3.up*3);
+        }
+    }
+}
