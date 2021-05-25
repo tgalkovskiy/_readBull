77f03d26f6fa7fe0b79b6e6ba2f6b3631058c844
Timur <tgalkovskiy@gmail.com>
2021-05-25 09:34:58 +0300

    newtraceanddrift

--- Assets/Scripts/Drift_Zone.cs
+++ Assets/Scripts/Drift_Zone.cs
@@ -4,12 +4,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using Cinemachine;
 
 public class Drift_Zone : MonoBehaviour
 {
     public AnimationCurve curve;
     private float time = 0;
-
+    CinemachineDollyCart cinemachineDollyCart;
+    private void OnTriggerEnter(Collider other)
+    {
+        cinemachineDollyCart = other.GetComponent<MainDir>().cinemachineDollyCart;
+        DOTween.To(() => cinemachineDollyCart.m_Speed, x => cinemachineDollyCart.m_Speed = x,
+            35, 0.5f);
+    }
     private void OnTriggerStay(Collider other)
     {
         Quaternion rotnow = Quaternion.Euler(0,curve.Evaluate(time),0);
@@ -20,6 +27,8 @@ public class Drift_Zone : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
+        DOTween.To(() => cinemachineDollyCart.m_Speed, x => cinemachineDollyCart.m_Speed = x,
+            50, 0.5f);
         other.transform.DOLocalRotate(Vector3.zero, 1);
     }
 }
