fce51c07509bf86a5dd02dc4250110eeca071de3
tgalkovskiy <80895083+GalkovskiyTimur@users.noreply.github.com>
2021-08-02 00:10:59 +0300

    fix table

--- Assets/Scripts/TableScore.cs
+++ Assets/Scripts/TableScore.cs
@@ -10,20 +10,22 @@ public class TableScore : MonoBehaviour
     private Text[] _score;
     private string[] _firstName = new string[] { "Luis", "Marc", "Julian", "Lina", "Sofi", "Clat", "Samanta", "Kate", "Roma" };
     private string[] _secondName = new string[] { "Poston", "Tic", "Pont", "Clus", "Sabj", "Onigo", "Rrown", "Igonch", "Spots" };
-    private int[] _scoreplayer; 
+    private int[] _scoreplayer;
+
     private void Awake()
     {
         if (_fieldText.Length > 0)
         {
-           _name = new Text[_fieldText.Length];
-           _score = new Text[_fieldText.Length];
+            _name = new Text[_fieldText.Length];
+            _score = new Text[_fieldText.Length];
             _scoreplayer = new int[_fieldText.Length];
-           for(int i =0;  i<_fieldText.Length; i++)
-           {
+            for (int i = 0; i < _fieldText.Length; i++)
+            {
                 _name[i] = _fieldText[i].transform.GetChild(0)?.GetComponent<Text>();
-                _name[i].text = $"{_firstName[Random.Range(0, _firstName.Length)]} {_secondName[Random.Range(0,_secondName.Length)]}";
+                _name[i].text =
+                    $"{_firstName[Random.Range(0, _firstName.Length)]} {_secondName[Random.Range(0, _secondName.Length)]}";
                 _score[i] = _fieldText[i].transform.GetChild(1)?.GetComponent<Text>();
-                _scoreplayer[i] = Random.Range(10000, 15000) - i*1000;
+                _scoreplayer[i] = Random.Range(10000, 15000) - i * 1000;
                 _score[i].text = _scoreplayer[i].ToString();
             }
         }
@@ -31,17 +33,17 @@ public class TableScore : MonoBehaviour
         {
             Destroy(this);
         }
-        Debug.Log(_fieldText.Length);
     }
 
     public void ShowCsore(int score)
     {
-        int pos = 5;
+        int pos = 6;
         for(int i =5; i>-1; i--)
         {       
             if (score > _scoreplayer[i])
             {
                 pos--;
+                Debug.Log(i);
             }
         }
         _name[pos].text = "Player";
