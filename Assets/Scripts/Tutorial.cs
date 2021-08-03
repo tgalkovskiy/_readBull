e90b41d6a1095d5ebb9f771ac53441ceefbe8def
tgalkovskiy <80895083+GalkovskiyTimur@users.noreply.github.com>
2021-08-03 23:11:42 +0300

    fix tutorial

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -14,6 +14,7 @@ public class Tutorial : MonoBehaviour
     [SerializeField] private GameObject panelTutor2 = default;
 
     public bool keytap = false;
+    public bool isTap = false;
     private void Start()
     {
         StartCoroutine(starttutor());
@@ -22,22 +23,36 @@ public class Tutorial : MonoBehaviour
     IEnumerator starttutor()
     {
         yield return new WaitForSeconds(delaystartfirsttutor);
-        panelTutor1.transform.DOMove(posfin.position, 0.6f).OnComplete(() => Time.timeScale = 0);
+        panelTutor1.transform.DOMove(posfin.position, 0.6f).OnComplete(() =>
+        {
+            Time.timeScale = 0;
+            isTap = true;
+        });
+        
         yield return new WaitUntil(() => keytap);
-        Time.timeScale = 1;
+        isTap = false;
         keytap = false;
+        Time.timeScale = 1;
         panelTutor1.transform.DOMove(posstart.position, 0.6f);
+        
         yield return new WaitForSeconds(delaystartfirstdrift);
-        panelTutor2.transform.DOMove(posfin.position, 0.6f).OnComplete(() => Time.timeScale = 0);
+        panelTutor2.transform.DOMove(posfin.position, 0.6f).OnComplete(() =>
+        {
+            Time.timeScale = 0;
+            isTap = true;
+        });
+
         yield return new WaitUntil(() => keytap);
         Time.timeScale = 1;
+        isTap = false;
         keytap = false;
         panelTutor2.transform.DOMove(posstart.position, 0.6f);
     }
 
     private void OnGUI()
     {
-        if (Event.current.Equals(Event.KeyboardEvent("A")) || Event.current.Equals(Event.KeyboardEvent("A")))
+        if(!isTap) return;
+        if (Event.current.Equals(Event.KeyboardEvent("A")) || Event.current.Equals(Event.KeyboardEvent("D")))
         {
             keytap = true;
         }
